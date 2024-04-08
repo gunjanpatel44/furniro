933f431a52c93ea4275329baf72c0ed666c47844
gunjanpatel44 <gunjanbhingaradiya@gmail.com>
2024-04-08 12:21:17 +0530

    resonsive updates

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,24 +4,28 @@ import { FaGreaterThan } from "react-icons/fa6";
 import Navbar from './Navbar';
 import Footer from './Footer';
 import FeatureBanner from './FeatureBanner';
+import Banner from '../img/preview-banner.svg'
 
 export default function Cart() {
   return (
-    <div>
+    <div className=' max-w-screen-2xl mx-auto'>
       <Navbar />
-      <div className='preview-banner'>
-        <div className='blur-background'>
-          <div className='cart-banner-div'>
-            <img src={Logo} alt='Logo'></img>
-            <p className='cart-banner text-3xl'>Cart</p>
-            <div className='flex flex-row content-center items-center'>
-              Home <FaGreaterThan /> cart
-            </div>
+      <div>
+        <div className='preview-banner'>
+        
+          <img src={Banner}/>
+        </div>
+        <div className='cart-banner-div'>
+          <img src={Logo} alt='Logo'></img>
+          <p className='cart-banner text-3xl'>Cart</p>
+          <div className='flex flex-row content-center items-center'>
+            Home <FaGreaterThan /> cart
           </div>
         </div>
       </div>
-      <FeatureBanner/>
-      <Footer/>
+      <FeatureBanner className=" relative"/>
+      <Footer />
     </div>
   )
 }
+c
